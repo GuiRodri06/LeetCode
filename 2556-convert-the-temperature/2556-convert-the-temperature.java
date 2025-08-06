2fa9c76004a2e1806496602ceb70af2bc4d7db27
Guilherme Rodrigues <guilhermeaz.rodrigues@gmail.com>
2025-08-06 21:01:33 +0100

    Time: 0 ms (100%), Space: 41.9 MB (59.67%) - LeetHub

--- 2556-convert-the-temperature/2556-convert-the-temperature.java
+++ 2556-convert-the-temperature/2556-convert-the-temperature.java
@@ -1,10 +1,5 @@
 class Solution {
     public double[] convertTemperature(double celsius) {
-        double[] ans = new double[2]; 
-        for (int i=0; i<=0; i++) {
-            ans[i] = celsius + 273.15;
-            ans[i+1] = celsius * 1.80 + 32.00;
-        }       
-        return ans;
+        return new double[] {celsius + 273.15, celsius * 1.80 + 32.00};
     }
 }
